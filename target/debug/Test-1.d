d33654f75c472df3689e8e1a2892549c4d4fd06c
fernandocmartinezmoyeda <144275819+fernandocmartinezmoyeda@users.noreply.github.com>
2024-08-29 03:56:46 +0000

    My first program

---
+++ target/debug/Test-1.d
@@ -0,0 +1,1 @@
+/workspaces/Test-1/target/debug/Test-1: /workspaces/Test-1/src/main.rs
---
+++ target/debug/deps/Test_1-b76bc6c677cf6fb5.d
@@ -0,0 +1,5 @@
+/workspaces/Test-1/target/debug/deps/Test_1-b76bc6c677cf6fb5: src/main.rs
+
+/workspaces/Test-1/target/debug/deps/Test_1-b76bc6c677cf6fb5.d: src/main.rs
+
+src/main.rs:
