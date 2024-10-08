c0add40de475898b453950277ad1ae142ef8b47e
fernando martinez <144275819+fernandocmtz@users.noreply.github.com>
2024-10-08 04:11:10 +0000

    Attendance Verification

---
+++ student_project/student_project/target/debug/deps/student_project-52f7a90d36ed8c05.d
@@ -0,0 +1,5 @@
+/workspaces/Test-1/student_project/student_project/target/debug/deps/student_project-52f7a90d36ed8c05: src/main.rs
+
+/workspaces/Test-1/student_project/student_project/target/debug/deps/student_project-52f7a90d36ed8c05.d: src/main.rs
+
+src/main.rs:
---
+++ student_project/student_project/target/debug/student_project.d
@@ -0,0 +1,1 @@
+/workspaces/Test-1/student_project/student_project/target/debug/student_project: /workspaces/Test-1/student_project/student_project/src/main.rs
